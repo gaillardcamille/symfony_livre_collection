9a8ed54c373914701a3df8a0f99200ef2b9cade4
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-25 22:16:49 +0100

    user crud et livre en admin

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/livre')]
+#[Route('/admin/livre')]
 class LivreController extends AbstractController
 {
     #[Route('/', name: 'app_livre_index', methods: ['GET'])]
