9032af345c7cb14df6b0ff099ab2ed5595437a96
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-25 22:11:31 +0100

    affichage livre spécial user

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
 use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,21 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard')]
-	public function index(): Response
+	public function index(LivreRepository $livreRepository): Response
 	{
 		return $this->render('dashboard/index.html.twig', [
 			'controller_name' => 'DashboardController',
-]);
+			'livres' => $livreRepository->findAll(),
+		]);
 	}
+
+    #[Route('/Livre/{id}', name: 'app_livre_for_user', methods: ['GET'])]
+	public function show_livre(Livre $livre,): Response
+	{
+		
+		return $this->render('dashboard/livre_for_user.html.twig', [
+			'livre' => $livre,
+		]);
+	}
+
 }
